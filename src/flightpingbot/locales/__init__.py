117f3932ef9cc03d5e1ffa40a7a7ebff910f7f4a
ferges123 <ferges123@users.noreply.github.com>
2026-08-24 17:53:12 +0200

    Split translations into locale modules

---
+++ src/flightpingbot/locales/__init__.py
@@ -0,0 +1,4 @@
+from .en import MESSAGES as EN_MESSAGES
+from .pl import MESSAGES as PL_MESSAGES
+
+__all__ = ["EN_MESSAGES", "PL_MESSAGES"]
--- tests/test_i18n.py
+++ tests/test_i18n.py
@@ -1,4 +1,5 @@
 from flightpingbot.i18n import t
+from flightpingbot.locales import EN_MESSAGES, PL_MESSAGES
 
 
 def test_t_accepts_language_as_format_value():
@@ -10,3 +11,7 @@ def test_t_accepts_language_as_format_value():
 
 def test_t_falls_back_to_english_for_unknown_language():
     assert t("xx", "cancelled") == "↩️ Cancelled. Nothing was changed."
+
+
+def test_all_locales_define_the_same_message_keys():
+    assert EN_MESSAGES.keys() == PL_MESSAGES.keys()
